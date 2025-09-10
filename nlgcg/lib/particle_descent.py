273585a6d8259f58cbd6736118b2d90dd5b1f1f0
arsen.hnatiuk <arsen.hnatiuk@outlook.com>
2025-09-10 17:41:52 +0200

    chore: conic particle descent testing

--- nlgcg/lib/particle_descent.py
+++ nlgcg/lib/particle_descent.py
@@ -15,9 +15,6 @@ class ParticleDescent:
     def __init__(
         self,
         m: int,
-        j_N: Callable,  # parameterized objective
-        grad_j_N: Callable,
-        hess_j_N: Callable,
         j: Callable,
         p: Callable,  # Dual variable
         grad_p: Callable,
@@ -37,9 +34,6 @@ class ParticleDescent:
     ):
         self.m = m
         self.j = j
-        self.j_N = j_N
-        self.grad_j_N = grad_j_N
-        self.hess_j_N = hess_j_N
         self.p = p
         self.grad_p = grad_p
         self.Omega = Omega
@@ -56,12 +50,6 @@ class ParticleDescent:
         self.f = f
         self.grad_f = grad_f
         self.hess_f = hess_f
-        # self.kernel_sign = np.hstack(
-        #     (
-        #         np.ones(self.m ** self.Omega.shape[0]),
-        #         -np.ones(self.m ** self.Omega.shape[0]),
-        #     )
-        # )
 
     def parameterize(self, r: np.ndarray, theta: np.ndarray) -> np.ndarray:
         matrix = np.zeros((len(r), 1 + self.Omega.shape[0]))
@@ -72,23 +60,13 @@ class ParticleDescent:
     def initial_distribution(self) -> tuple:
         r = np.ones(self.m ** self.Omega.shape[0])
         r = np.hstack((r, -r))
-        theta = (
-            np.array(
-                np.meshgrid(
-                    *(
-                        np.linspace(bound[0], bound[1], self.m + 2, endpoint=True)[1:-1]
-                        for bound in self.Omega
-                    )
-                )
-            )
-            .reshape(len(self.Omega), -1)
-            .T
-        )
+        grids_1d = [np.logspace(-4, 0, self.m + 2)[1:-1]]
+        grids_1d += [
+            np.linspace(bound[0], bound[1], self.m + 2, endpoint=True)[1:-1]
+            for bound in self.Omega[1:]
+        ]
+        theta = np.array(np.meshgrid(*(grids_1d))).reshape(len(self.Omega), -1).T
         theta = np.vstack((theta, theta))  # Positive and negative support
-        theta = np.array(
-            [[0.06, 0.28], [0.05, 0.51], [0.04, 0.71]]
-        ) + 0.01 * np.random.normal(size=(3, 2))
-        r = np.array([1, -0.7, 0.8]) + 0.01 * np.random.normal(size=(3))
         c = 0
         self.kernel_sign = np.sign(r)
         return r, theta, c
@@ -121,67 +99,80 @@ class ParticleDescent:
             maximum_iterations=self.ssn_steps,
         )
         ssn_solution = ssn.solve(tol=self.machine_precision, u_0=u_0)
-        ssn_normal = ssn_solution.copy()
-        c_plus_normal = ssn_normal[0]
-        u_plus_normal = u.copy()
-        tuples = [
-            (u_plus_normal, c_plus_normal),
-            (u, c),
-        ]
-        values = [self.j(u, c) for u, c in tuples]
+        c_plus = ssn_solution[0]
+        cs = [c, c_plus]
+        values = [self.j(u, local_c) for local_c in cs]
         best_value = np.argmin(values)
-        u_plus, c_plus = tuples[best_value]
-        return c_plus
+        c_best = cs[best_value]
+        return c_best
 
     def retraction(
-        self, r: np.ndarray, del_r: np.ndarray, theta: np.ndarray, del_theta: np.ndarray
+        self,
+        r: np.ndarray,
+        del_r: np.ndarray,
+        theta: np.ndarray,
+        del_theta: np.ndarray,
+        mode: str = "canonical",
     ) -> tuple:
         # logging.info(f"{r.shape}, {del_r.shape}, {theta.shape}, {del_theta.shape}")
-        r_retraction = r + del_r
-        theta_retraction = theta + del_theta
+        if mode == "canonical":
+            r_retraction = r + del_r
+            theta_retraction = theta + del_theta
+        elif mode == "mirror":
+            r_retraction = r * np.exp(del_r)  # /r
+            theta_retraction = theta + del_theta
         return r_retraction, theta_retraction
 
     def solve(
         self,
         max_iters: int = 1000,
+        u_0: Measure = Measure(),
+        c_0: float = 0,
     ):
-        r, theta, c = self.initial_distribution()
+        if len(u_0.coefficients):
+            self.kernel_sign = np.sign(u_0.coefficients)
+            r = self.kernel_sign * np.sqrt(
+                self.kernel_sign * u_0.coefficients * len(u_0.coefficients)
+            )
+            theta = u_0.support
+            c = c_0
+        else:
+            r, theta, c = self.initial_distribution()
+        params = self.parameterize(r, theta).reshape(-1, 1 + self.Omega.shape[0])
         u = Measure(matrix=self.parameterize(r, theta))
-        c = self.finite_dimensional_step(u, c)
+        # c = self.finite_dimensional_step(u, c)
         logging.info(f"0: objective {self.j(u, c):.14E}")
-        logging.info(r)
-        logging.info(theta)
+        objective_values = [self.j(u, c)]
+        # logging.info(params)
         for iter in range(max_iters):
             p_u = self.p(u, c)
             grad_p_u = self.grad_p(u, c)
-            r_update = -2 * self.a_parameter * np.abs(r) * (-p_u(theta) + self.alpha)
-            # logging.info(p_u(theta))
-            # grad = np.array(
-            #     self.grad_j_N(np.hstack((self.parameterize(r, theta).flatten(), c)))
-            # )
-            # hess = np.array(
-            #     self.hess_j_N(
-            #         np.hstack((np.hstack((r.reshape(-1, 1), theta)).flatten(), c))
-            #     )
-            # )
-            # dir = -np.linalg.solve(hess, grad)
-            # logging.info(grad[:-1].reshape(len(r), -1))
-            # r_update = -self.a_parameter * grad[:-1].reshape(len(r), -1)[:, 0]
-            # theta_update = -self.b_parameter * grad[:-1].reshape(len(r), -1)[:, 1:]
-            theta_update = self.b_parameter * np.array(grad_p_u(theta))
-            # logging.info("----------------------")
-            # logging.info(-grad)
-            # logging.info(r_update / (2 * self.a_parameter * np.abs(r)))
-            # logging.info(
-            #     np.multiply(theta_update.T, r**2).T / (self.b_parameter * len(r))
-            # )
-            # logging.info(f"---------------------")
-            r, theta = self.retraction(r, r_update, theta, theta_update)
-            theta[:, 0] = np.maximum(theta[:, 0], 1e-5)
+            r_update = (
+                -2
+                * self.a_parameter
+                # * r
+                * (-self.kernel_sign * p_u(theta) + self.alpha)
+            )
+            theta_update = (
+                self.b_parameter * self.kernel_sign * np.array(grad_p_u(theta)).T
+            ).T
+            r, theta = self.retraction(r, r_update, theta, theta_update, mode="mirror")
+            keep_indices = np.logical_and(theta[:, 0] > 1e-5, np.abs(r) > 1e-4)
+            r = r[keep_indices]
+            self.kernel_sign = self.kernel_sign[keep_indices]
+            theta = theta[keep_indices]
+            # theta[:, 0] = np.maximum(theta[:, 0], 1e-5)
+            params = self.parameterize(r, theta).reshape(-1, 1 + self.Omega.shape[0])
             u = Measure(matrix=self.parameterize(r, theta))
-            c = self.finite_dimensional_step(u, c)
-            if iter % 100 == 0:
-                logging.info(f"{iter + 1}: objective {self.j(u, c):.14E}")
-                logging.info(r)
-                logging.info(theta)
-        return u, c
+            # c = self.finite_dimensional_step(u, c)
+            objective_values.append(self.j(u, c))
+            if (iter + 1) % 100 == 0:
+                logging.info(
+                    f"{iter + 1}: supp: {len(u.coefficients)}, objective {self.j(u, c):.14E}"
+                )
+                # self.b_parameter = min(self.a_parameter, self.b_parameter * 1.002)
+                # self.a_parameter *= 1.001
+                # logging.info(f"a: {self.a_parameter}, b:{self.b_parameter}")
+                # logging.info(f"min: {np.min(np.abs(r))}, max: {np.max(np.abs(r))}")
+                # logging.info(params)
+        return u, c, objective_values
