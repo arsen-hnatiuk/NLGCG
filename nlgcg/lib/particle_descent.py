40888c463ee77fac2b4f946eba43156f24eb652c
arsen-hnatiuk <arsen.hnatiuk@hu-berlin.de>
2026-02-20 17:28:27 +0100

    fix attempt: reformulated model prediction

--- nlgcg/lib/particle_descent.py
+++ nlgcg/lib/particle_descent.py
@@ -37,7 +37,7 @@ class ParticleDescent:
         ssn_steps: int = 100,
         do_linesearch: bool = True,
         do_pruning: bool = False,
-        residual_tolerance: float = 5e-14
+        residual_tolerance: float = 5e-14,
     ):
         self.m = m
         self.j = j
@@ -60,7 +60,9 @@ class ParticleDescent:
         self.do_linesearch = do_linesearch
         self.do_pruning = do_pruning
 
-    def parameterize(self, r: np.ndarray, theta: np.ndarray, Nparticle: int = None) -> np.ndarray:
+    def parameterize(
+        self, r: np.ndarray, theta: np.ndarray, Nparticle: int = None
+    ) -> np.ndarray:
         if Nparticle is None:
             Nparticle = len(r)
         matrix = np.zeros((len(r), 1 + self.Omega.shape[0]))
@@ -157,123 +159,132 @@ class ParticleDescent:
 
         # Initialize
         u = Measure(matrix=parameterize(r, theta))
-        c = np.sum(np.sign(cs) * cs ** 2) / Nparticle
+        c = np.sum(np.sign(cs) * cs**2) / Nparticle
         logging.info(f"0: objective {self.j(u, c):.14E}")
         objective_values = [self.j(u, c)]
         times = [time.perf_counter() - t_0]
         supports = [len(u.coefficients)]
 
-        p_def = 0
-        innr = 0
-        update = 0
-        retr = 0
-        post = 0
-        ut = 0
-
         min_a_parameter = self.a_parameter
 
         for it in range(max_iters):
-            t = time.perf_counter()
             p_u = self.p(u, c)
             grad_p_u = self.grad_p(u, c)
-            p_def += time.perf_counter() - t
 
-            t = time.perf_counter()
             inner = c * np.ones(self.constant_dim)
             if len(u.coefficients):
                 inner += self.kernel(u.support).T @ u.coefficients
-            inner = - self.grad_f(inner)
-            innr += time.perf_counter() - t
-
-            t = time.perf_counter()
-
-            #mask = self.a_parameter > min_a_parameter * 1.1
-            r_update = (
-                self.a_parameter * 2. * (self.kernel_sign * p_u(theta) + self.alpha)
-            )
+            inner = -self.grad_f(inner)
             inner_sum = np.sum(inner)
-            cs_update = (
-                self.a_parameter * 2. * np.sign(cs) * inner_sum
+
+            inner_r_update = self.kernel_sign * p_u(theta) + self.alpha
+            inner_cs_update = np.sign(cs) * inner_sum
+            inner_theta_update = (self.kernel_sign * np.array(grad_p_u(theta)).T).T
+
+            # mask = self.a_parameter > min_a_parameter * 1.1
+            r_update = self.a_parameter * 2.0 * inner_r_update
+            cs_update = self.a_parameter * 2.0 * inner_cs_update
+            theta_update = self.b_parameter * inner_theta_update
+
+            # Descent, predicted by the gradient
+            r_pred = 2 * (r * r_update) @ (r * inner_r_update)
+            cs_pred = 2 * (cs * cs_update) @ (cs * inner_cs_update)
+            theta_pred = np.sum(
+                [
+                    r_local**2 * theta_update_local @ inner_theta_update_local
+                    for r_local, theta_update_local, inner_theta_update_local in zip(
+                        r, theta_update, inner_theta_update
+                    )
+                ]
             )
-            theta_update = (
-                self.b_parameter * self.kernel_sign * np.array(grad_p_u(theta)).T
-            ).T
-            update += time.perf_counter() - t
+            pred = (r_pred + cs_pred + theta_pred) / Nparticle
 
             r_old, cs_old, theta_old = r.copy(), cs.copy(), theta.copy()
-            decrease = 1.
-            decrease_tol = 0.
+            decrease = 1.0
+            decrease_tol = 0.0
             while decrease > decrease_tol:
-                t = time.perf_counter()
                 r, cs, theta = self.retraction(
-                    r_old, r_update, cs_old, cs_update, theta_old, theta_update, mode="mirror"
+                    r_old,
+                    r_update,
+                    cs_old,
+                    cs_update,
+                    theta_old,
+                    theta_update,
+                    mode="mirror",
                 )
-                retr += time.perf_counter() - t
 
-                t = time.perf_counter()
-                c = np.sum(np.sign(cs) * cs ** 2) / Nparticle
-                post += time.perf_counter() - t
-
-                t = time.perf_counter()
+                c = np.sum(np.sign(cs) * cs**2) / Nparticle
                 u = Measure(matrix=parameterize(r, theta))
                 obj = self.j(u, c)
-                ut += time.perf_counter() - t
 
                 if self.do_linesearch:
-                    r_grad = (1 / Nparticle) * (r_old * r_update) / self.a_parameter
-                    cs_grad = (1 / Nparticle) * (cs_old * cs_update) / self.a_parameter
-                    theta_grad = (1 / Nparticle) * (theta_update * r_old[:, None]**2) / self.b_parameter
-                    pred = r_grad.dot(r - r_old) + cs_grad.dot(cs - cs_old) + (
-                        theta_grad.reshape(-1).dot((theta - theta_old).reshape(-1)))
+                    # r_grad = (1 / Nparticle) * (r_old * r_update) / self.a_parameter
+                    # cs_grad = (1 / Nparticle) * (cs_old * cs_update) / self.a_parameter
+                    # theta_grad = (
+                    #     (1 / Nparticle)
+                    #     * (theta_update * r_old[:, None] ** 2)
+                    #     / self.b_parameter
+                    # )
+                    # pred = (
+                    #     r_grad.dot(r - r_old)
+                    #     + cs_grad.dot(cs - cs_old)
+                    #     + (theta_grad.reshape(-1).dot((theta - theta_old).reshape(-1)))
+                    # )
 
-                    #TODO: bug, this linesearch does not terminate always
-                    pred_factor = .9
+                    # TODO: bug, this linesearch does not terminate always
+                    pred_factor = 0.9
                     model = objective_values[-1] - pred_factor * pred
-                    print(f"{it}, {self.a_parameter:1.2e}: red={objective_values[-1] - obj:1.3e} pred={pred:1.3e}")
+                    logging.info(
+                        f"{it}, {self.a_parameter:1.2e}: red={objective_values[-1] - obj:1.3e} pred={pred:1.3e}"
+                    )
 
                     decrease = obj - model
-                    ls_fact = 1.
+                    ls_fact = 1.0
                     if decrease >= decrease_tol:
-                        contraction = max(self.a_parameter / (1 + ls_fact), min_a_parameter) / self.a_parameter
+                        contraction = (
+                            max(self.a_parameter / (1 + ls_fact), min_a_parameter)
+                            / self.a_parameter
+                        )
                         self.a_parameter = self.a_parameter * contraction
                         self.b_parameter = self.b_parameter * contraction
                         r_update = r_update * contraction
                         cs_update = cs_update * contraction
                         theta_update = theta_update * contraction
+                        pred = pred * contraction
 
-                        if contraction >= .99:
+                        if contraction >= 0.99:
                             # exit line-search and accept step
-                            logging.warn(f"line-search failed in iteration {it}: descent is {decrease}, red={objective_values[-1] - obj:1.3e}, pred={pred:1.3e}, ")
-                            #breakpoint()
+                            logging.warn(
+                                f"line-search failed in iteration {it}: descent is {decrease}, red={objective_values[-1] - obj:1.3e}, pred={pred:1.3e}, "
+                            )
+                            # breakpoint()
                             decrease = 0
                     else:
-                        if decrease < 0.:
+                        if decrease < 0.0:
                             self.a_parameter = self.a_parameter * (1 + 0.25 * ls_fact)
                             self.b_parameter = self.b_parameter * (1 + 0.25 * ls_fact)
 
                 else:
                     # just accept the step, and do not check for descent
-                    decrease = -1.
+                    decrease = -1.0
 
             keep_indices = np.logical_and(theta[:, 0] > 1e-6, np.abs(r) > 1e-6)
 
             if self.do_pruning and not np.all(keep_indices):
-                t = time.perf_counter()
                 dropped_ind = np.logical_not(keep_indices)
                 r_drop = r[dropped_ind]
                 theta_drop = theta[dropped_ind]
                 r = r[keep_indices]
                 theta = theta[keep_indices]
                 self.kernel_sign = self.kernel_sign[keep_indices]
-                post += time.perf_counter() - t
 
-                t = time.perf_counter()
                 u = Measure(matrix=parameterize(r, theta))
                 old_obj = obj
                 obj = self.j(u, c)
-                ut += time.perf_counter() - t
 
-                logging.info(f"dropped indices {np.where(dropped_ind)[0]} with r={r_drop} and theta={theta_drop}, function change {obj - old_obj}")
+                logging.info(
+                    f"dropped indices {np.where(dropped_ind)[0]} with r={r_drop} and theta={theta_drop}, function change {obj - old_obj}"
+                )
 
             times.append(time.perf_counter() - t_0)
 
@@ -283,7 +294,8 @@ class ParticleDescent:
                 logging.info("Divergence")
                 return u, c, objective_values, supports, times, False
             elif (
-                np.max(np.abs(obj - np.array(objective_values[-101:]))) < self.residual_tolerance
+                np.max(np.abs(obj - np.array(objective_values[-101:])))
+                < self.residual_tolerance
             ):
                 logging.info("Convergence")
                 return u, c, objective_values, supports, times, True
@@ -297,22 +309,12 @@ class ParticleDescent:
 
             if (it + 1) % 1000 == 0:
                 logging.info(obj - np.max(objective_values[-101:-1]))
-                # logging.info(np.max(np.abs(obj - np.array(objective_values[-101:]))))
                 logging.info(
                     f"r_delta: {np.linalg.norm(r_update):.3E}, theta_delta: {np.linalg.norm(theta_update):.3E}, c_update: {np.linalg.norm(cs_update):.3E}"
                 )
                 logging.info(
-                    f"p: {p_def:.3E}, inner: {innr:.3E}, update: {update:.3E}, retr: {retr:.3E}, post: {post:.3E}, ut: {ut:.3E}"
-                )
-                logging.info(
                     f"{it + 1}: supp: {keep_indices.sum()}, c value: {c:.3E}, objective {obj:.14E}"
                 )
-                p_def = 0
-                innr = 0
-                update = 0
-                retr = 0
-                post = 0
-                ut = 0
             if time.perf_counter() - t_0 > max_time:
                 logging.info("Max time reached")
                 break
